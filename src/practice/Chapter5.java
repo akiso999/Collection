c4f550a6a150e6cda6a2083d5048d82f68f4ae03
akiikoisoe <180363344+akiso999@users.noreply.github.com>
2024-09-29 11:03:06 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,12 +1,31 @@
 
 package practice;
 
-public class Chapter5{
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
-  public static void main(String[] args) {
+public class Chapter5 {
 
-    // プログラムを作成
+		public static void main(String[] args) {
+			
+			List<Task> list = new LinkedList<>() {
+				{
+					add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
+					add(new Task(LocalDate.of(2021, 9, 15), "○○社面接"));
+					add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
+					add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
+					add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
+				}
+				};
 
-  }
+				Collections.sort(list);
 
+				for (int i = 0; i < list.size(); i++) {
+					System.out.println(list.get(i));
+				}
+
+
+		}
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,43 @@
+
 package practice;
 
-public class Task {
+import java.time.LocalDate;
 
+public class Task implements Comparable<Task>{
+
+	private LocalDate date;
+	private String task;
+
+	public Task(LocalDate date,String task) {
+		this.date = date;
+		this.task = task;
+	}
+	
+//	public LocalDate getDate() {
+//		return date;
+//	}
+
+//	public String getTask() {
+//		return task;
+//	}
+	
+//	public void setDate(LocalDate date) {
+//		this.date = date;
+//	}
+
+//	public void setTask(String task) {
+//		this.task = task;
+//	}
+		
+	// compareToメソッドをオーバーライドする
+	// 日付で並び替えるため、LocalDateのcompareToを使う
+	@Override
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
+	}
+
+	@Override
+	public String toString() {
+	return this.date + ":" + this.task;
+	}
 }
